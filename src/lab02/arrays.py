fd07c25ba88f9c1a24a9f195e8f838d5a68dd334
dspnt <katik222008@gmail.com>
2025-10-10 15:02:25 +0300

    2

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,6 +1,4 @@
 def min_max(nums: list[float or int]) -> tuple[float or int, float or int]:
-    if not isinstance(nums, list):
-        raise TypeError("TypeError")
     if len(nums) == 0:
         raise ValueError("ValueError")
     mn = min(nums)
@@ -8,8 +6,6 @@ def min_max(nums: list[float or int]) -> tuple[float or int, float or int]:
     return(mn,mx)
 
 def unique_sorted(nums: list[float or int]) -> list[float or int]:
-    if not isinstance(nums, list):
-        raise TypeError("TypeError")
     nums = set(nums)
     nums = list(nums)
     nums.sort()
@@ -17,15 +13,16 @@ def unique_sorted(nums: list[float or int]) -> list[float or int]:
 
 def flatten(mat: list[list or tuple]) -> list:
     a = []
-    if not isinstance(mat, list):
-        raise TypeError("TypeError")
     for i in mat:
         if not isinstance(i, (list, set, tuple)):
             raise TypeError("TypeError")
         for b in i:
             a.append(b)
     return(a)
-
+print(flatten([[1, 2], [3, 4]]))
+print(flatten([[1, 2], (3, 4, 5)]))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
 
 
 
