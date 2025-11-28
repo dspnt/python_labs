673c082210d99292759454a225c95f5dcd8a5d58
dspnt <katik222008@gmail.com>
2025-11-28 13:00:17 +0300

    <3

---
+++ src/lab06/cli_convert.py
@@ -0,0 +1,31 @@
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = build_parser()
+    
+    if not argv or any(arg in argv for arg in ('-h', '--help')):
+        parser.print_help()
+        return
+        
+    args = parser.parse_args(argv)
+
+    command_handlers = {
+        'json2csv': json_to_csv,
+        'csv2json': csv_to_json,
+        'csv2xlsx': csv_to_xlsx
+    }
+
+    try:
+        if not os.path.exists(args.input):
+            raise FileNotFoundError(f"Файл не найден: {args.input}")
+
+        command_handlers[args.command](args.input, args.output)
+        print(f"Файл создан: {args.output}")
+        
+    except FileNotFoundError as e:
+        print(f"Ошибка: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Ошибка конвертации: {e}")
+        sys.exit(1)
